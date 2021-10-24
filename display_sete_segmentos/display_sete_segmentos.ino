44b8477eb6a37f4462f96b3061f84a16f33e016e
Projetuino <projetuino@outlook.com>
2021-10-24 11:20:23 -0300

    Diminuição Código

--- display_sete_segmentos/display_sete_segmentos.ino
+++ display_sete_segmentos/display_sete_segmentos.ino
@@ -1,6 +1,6 @@
 //Mapeamento dos pinos do display na
 //sequencia {a, b, c, d, e, f, g, DP}
-const int pinos[8] = {4, 5, 10, 9, 8, 3, 2, 11};
+const int pinos[8] = {5, 4, 9, 10, 11, 6, 7, 8};
 const int numeros[10][8] = {
     {1, 1, 1, 1, 1, 1, 0, 0}, 
     {0, 1, 1, 0, 0, 0, 0, 0},
@@ -27,14 +27,7 @@ void loop() {
 }
 
 void mostrarNumero(int num){
-  apagarDisplay();
   for(int i = 0; i < 8; i++){
     digitalWrite(pinos[i], numeros[num][i]);
-  }
-}
-
-void apagarDisplay(){
-  for(int i = 0; i < 8; i++){
-    digitalWrite(pinos[i], LOW);
   }
 }
